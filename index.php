f2c0721687931391c6ab8aaef7435138dbc002e2
DushyantMainwal <dushyantmainwal@outlook.com>
2018-04-02 18:03:52 +0530

    post formats

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ get_header();
 if (have_posts()):
     while (have_posts()): the_post() ?>
 
-        <?php get_template_part('extra_content') ?>
+        <?php get_template_part('extra_content', get_post_format()) ?>
 
     <?php endwhile;
 else: echo "No Post Exist";
